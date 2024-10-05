c10db17c7b4d859b2840cdb7af30315cb09f0993
Cadu7 <cadu.carvalho@best2bee.com>
2024-10-05 15:51:29 -0300

    configuração dos testes

---
+++ applications/node_ts/src/test/mock/prisma.ts
@@ -0,0 +1,20 @@
+import "reflect-metadata"
+import {beforeEach, MockedObject, vi} from "vitest";
+import {customer, list, user} from "../../app/repository/Prisma";
+import {Prisma} from "@prisma/client";
+
+vi.mock("../../app/repository/Prisma", () => ({
+    user: {
+        findFirst: vi.fn(),
+        create: vi.fn()
+    },
+    list: {},
+    customer: {
+        create: vi.fn()
+    }
+}))
+
+export const userMock: MockedObject<Prisma.UserDelegate> = vi.mocked(user);
+export const customerMock: MockedObject<Prisma.CustomerDelegate> = vi.mocked(customer);
+export const listMock: MockedObject<Prisma.ListDelegate> = vi.mocked(list);
+beforeEach(() => vi.resetAllMocks())
